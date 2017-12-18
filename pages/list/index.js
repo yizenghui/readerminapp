74bbcb57f40b86ad440adef2cf7e11f242339932
Administrator <245561237@qq.com>
2017-12-18 18:00:14 +0800

    ss

--- pages/list/index.js
+++ pages/list/index.js
@@ -43,6 +43,7 @@ Page({
 
   userSign:function(){
     if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo)
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
@@ -51,6 +52,8 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        
+        console.log(res.userInfo)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -61,6 +64,7 @@ Page({
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
+          console.log('jy',res.userInfo)
           this.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
