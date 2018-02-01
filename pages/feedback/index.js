fe6ff8dc32c42057c2ee6200314f02694b24ea8f
yizenghui <zenghuitrue@gmail.com>
2018-02-01 23:31:31 +0800

    增加反馈功能，界面细节调整

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,11 +1,13 @@
 // pages/feedback/index.js
+
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    problem:""
   },
 
   /**
@@ -21,46 +23,76 @@ Page({
   onReady: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  bindKeyInput: function (e) {
+    this.setData({
+      problem: e.detail.value
+    })
   },
+  // 订阅提交
+  formPost: function (e) {
+    var that = this
+    var formId = e.detail.formId
+    var problem = that.data.problem
+    wx.showModal({
+      title: '成功反馈',
+      content: '已经提交,处理结果将以模板消息回复！',
+      showCancel: false
+    })
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    wx.request({
+      url: 'https://minapp.readfollow.com/feedback',
+      method: 'POST', 
+      data: {
+        openid: app.globalData.openID,
+        problem: problem,
+        formid: formId,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      fail: function () {
+
+      },
+      success: function (res) {
+        that.setData({
+          subcribe_status: res.data.Status
+        })
+        console.log(res)
+      }
+    })
+    console.log('form发生了submit事件，携带数据为：', e.detail.formId)
   },
+  Post: function (e) {
+    var that = this
+    var formId = e.detail.formId
+    var urlStr = that.data.url
+    wx.showModal({
+      title: '成功订阅',
+      content: '7天内通知一次更新情况',
+      showCancel: false
+    })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+    wx.request({
+      url: 'https://minapp.readfollow.com/subscribe',
+      // url: 'https://localhost:1323/list', 
+      method:"POST",
+      data: {
+        openid: app.globalData.openID,
+        url: urlStr,
+        formid: formId,
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      fail: function () {
+
+      },
+      success: function (res) {
+        that.setData({
+          subcribe_status: res.data.Status
+        })
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
